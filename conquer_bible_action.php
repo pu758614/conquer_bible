ef9e08087c77830c73eb756983f173b66e8ea0f8
咖哩呦 <galio.huang@hgiga.com>
2020-02-11 22:37:00 +0800

    make it better

--- conquer_bible_action.php
+++ conquer_bible_action.php
@@ -4,14 +4,14 @@ include_once("$ROOT_PATH/lib/common.php");
 include_once("$ROOT_PATH/lib/db_lib.php");
 // print_r(dirname(__DIR__));
 $db = NEW db_lib;
-
-$re = $db->getArrayById('sercice','id','2');
-$msg = isset($_POST['msg'])?trim($_POST['msg']):'';
-$user_uuid = isset($_POST['user_uuid'])?trim($_POST['user_uuid']):'';
-$user_info = $db->getUserInfo($user_uuid,'uuid');
-if($user_info == array()){
-    $db->addLineUser('U7024af33ac34455f97b39b7bee8b8436');
-}
+echo "1111";
+// $re = $db->getArrayById('sercice','id','2');
+// $msg = isset($_POST['msg'])?trim($_POST['msg']):'';
+// $user_uuid = isset($_POST['user_uuid'])?trim($_POST['user_uuid']):'';
+// $user_info = $db->getUserInfo($user_uuid,'uuid');
+// if($user_info == array()){
+//     $db->addLineUser('U7024af33ac34455f97b39b7bee8b8436');
+// }
 // switch ($msg) {
 //     case '進攻':
 // 
@@ -21,6 +21,6 @@ if($user_info == array()){
 //         // code...
 //         break;
 // }
-pr($user_info);
+//pr($user_info);
 
 ?>
